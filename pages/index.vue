3c72299bca985fcac0a92e4ff20ac90bf06a2742
blargoflatsome <jordan.buermann@gmail.com>
2022-07-15 09:13:26 -0700

    test commit on new pc

--- pages/index.vue
+++ pages/index.vue
@@ -1,13 +1,36 @@
-<template>
-  <main>
-    <section class="self-center flex flex-col flex-1 items-center justify-center">
-      <h1 class="title text-center">Nuxt — Tailwind — Netlify CMS</h1>
-      <h2 class="subtitle text-center">Boilerplate</h2>
-    </section>
+<template lang="pug">
+main
+  section
+    h1(class="title text-center") 4Shame Not Honor!
+    h2(class="subtitle text-center") Something something oops
+    p To Do
+    ul
+      li SEO head stuff
+      li Menu should close on link click/router change
+      li Remove/cleanup all nuxt base template css crud
+      li Table / Tabs widget
+      li Update logo
+      li fix all header/footer links 
+      li Decide how to layout Player Profiles vs Character Bio's 
+       ul 
+        li Should Player profiles just be inside the About Us page?
+      li subnav?
 
-    <section class="mt-8">
-      <h3 class="text-primary-600 dark:text-primary-400 max-w-5xl mx-auto">Latest blog post</h3>
-      <posts post-type="blog" :amount="1" />
-    </section>
-  </main>
+  h2 Tabs
+  p Create a widget that sets a Title/Content for each table 
+  p The tabs then hide/reveal/side scroll? 
+  p Or always visible in order, but scroll down/up 
+  p Look at work code to make them update by user scrolling 
+
+  h2 Table 
+  p Can you set table css rules and just use .md table stuff?
+  p or do you need to make a widget?
+
+  h2 Blockquote?
 </template>
+
+<script>
+export default {
+  name: "Homepage"
+}
+</script>
