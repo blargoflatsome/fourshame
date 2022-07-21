901eea3684ec575b7d89ec40bd4db9a638a0bd3d
blargoflatsome <jordan.buermann@gmail.com>
2022-07-20 17:23:19 -0700

    page styling

--- pages/characters/index.vue
+++ pages/characters/index.vue
@@ -2,7 +2,21 @@
 main
   section( v-if="posts" class="w-full max-w-5xl mx-auto")
     h1( class="title") Characters
-    posts(:posts="posts" key="characters" pathPrefix="characters")
+    div(class="flex justify-evenly")
+      div(
+        v-for="(post,index) in posts" 
+        :key="index"
+        class="flex-1"
+      )
+        div(class="flex")
+          img(:src='post.thumbnail' width="200" height="200" class="card-image  border-4 border-accent-dark")
+          div(class="flex-col my-4 md:my-0 md:ml-4 leading-snug")
+            h2(class="card-title") {{ post.title || post.name }}
+            h3(class="leading-none") {{ post.subtitle }}
+        div(class="flex-col")
+          nuxt-content(:document="{ body: post.excerpt || post.body }" class="max-h-80 overflow-hidden")
+          nuxt-link(type="button" :to="`/characters/${post.slug}`" class="btn mb-3 text-center block w-full") Learn More
+
 </template>
 
 <script>
--- pages/players/index.vue
+++ pages/players/index.vue
@@ -1,12 +1,20 @@
 <template lang="pug">
 main
   section( v-if="posts" class="w-full max-w-5xl mx-auto")
-    h1( class="title") The Cast
-    posts(:posts="posts" key="players" pathPrefix="players")
+    h1( class="title") The Cast of 4ShameNotHonor
+    div(class="cards grid grid-cols-2 gap-4")
+      div(
+        v-for="(post,index) in posts" 
+        :key="index"
+        class="card"
+      )
+        img(v-if="post.thumbnail" :src='post.thumbnail' width="200" height="200" class="border-4 border-accent-dark")
+        div(class="flex-col my-4 md:my-0 md:ml-4 leading-snug")
+          h2(class="card-title") {{ post.title || post.name }}
+          nuxt-content(:document="post")
 </template>
 
 <script>
-// Display cards of Players, with no child detail pages
 export default {
   async asyncData({ $content, error }) {
     let posts;
